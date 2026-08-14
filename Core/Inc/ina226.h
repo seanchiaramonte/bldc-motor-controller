98bde95a8e41381f1778735652b6c8dea6debf5d
Sean Chiaramonte <seanchiaramonte@gmail.com>
2026-08-14 00:41:13 -0400

    feat(ina226): add INA226_ReadCurrent and return status from INA226_Initialize()
    - Read the raw current from the 0x04 register using HAL_I2C_Mem_Read
    
    - Assemble the raw current value by casting the bytes into a signed 16 bit integer, scaled to milliamps and return as a float output parameter
    
    - Return HAL_OK if the read operation succeeds and return status otherwise
    
    - Change the return type of INA226_Initialize to HAL_StatusTypeDef, so status can now be returned instead of printed

--- Core/Inc/ina226.h
+++ Core/Inc/ina226.h
@@ -1,19 +1,36 @@
 #ifndef INA226_H
 #define INA226_H
 
-#include "stm32f4xx_hal.h"
-#include <stdio.h>
+#include "i2c.h"
+#include <stdint.h>
+
 /** 
- * @brief Writes the calibration and configuration values to the INA226.
+ * @brief Writes the calibration and configuration values to the INA226 over the I2C1 bus.
  * 
  * @details 
  * Configures the INA226 for current measurement, writing the value to the 0x00 register. Calculates the calibration value 
- * (0.1 ohm shunt and 0.1mA/bit resolution) and writes that to the 0x05 register. The function has no return value, so 
- * I2C failures are printed over UART.
+ * (0.1 ohm shunt and 0.1mA/bit resolution) and writes that to the 0x05 register. 
  * 
- * @note Function should be called once at boot and I2C1 must be initialized before calling it
+ * @note Function should be called once at boot and I2C1 must be initialized before calling it.
+ * 
+ * @return HAL_OK if the write operation was successful, or if it failed, a HAL error code (HAL_ERROR, HAL_BUSY, HAL_TIMEOUT).
 */
-void INA226_Initialize(void);
+HAL_StatusTypeDef INA226_Initialize(void);
 
+/** 
+ * @brief Reads the scaled milliamp current from the INA226 over the STM32's I2C1 bus.
+ * 
+ * @details 
+ * Reads the raw current value from the INA226's 0x04 register. The function reassembles the two bytes and casts that value to
+ * a 16-bit signed integer (because current direction is meaningful) which is multiplied by the LSB 
+ * and 1000 so that current is outputted in mA.
+ * 
+ * @note Must be called after INA226_Initialize().
+ * 
+ * @param[out] current Pointer to a float where the read current will be stored. No modification if failure occurs.
+ * 
+ * @return HAL_OK if the read operation was successful, or if it failed, a HAL error code (HAL_ERROR, HAL_BUSY, HAL_TIMEOUT).
+*/
+HAL_StatusTypeDef INA226_ReadCurrent(float *current);
 
 #endif /* INA226_H */
--- Core/Src/ina226.c
+++ Core/Src/ina226.c
@@ -5,7 +5,7 @@
 #define CURRENT_LSB 0.0001f // 1A max / 32768 (signed 16 bit reg.) ≈ 0.00003051 rounded up to 0.0001 = 0.1mA per bit
 
 // See ina226.h for function documentation
-void INA226_Initialize(void)
+HAL_StatusTypeDef INA226_Initialize(void)
 {
     HAL_StatusTypeDef status;
 
@@ -31,7 +31,7 @@ void INA226_Initialize(void)
 
     if (status != HAL_OK)
     {
-        printf("Calibration Write Error:%d", status); // Indicates write error (especially useful since no return value is used)
+        return status; // Indicates write error
     }
 
     // Configuration write
@@ -39,6 +39,25 @@ void INA226_Initialize(void)
 
     if (status != HAL_OK)
     {
-        printf("Configuration Write Error:%d", status);
+        return status;
     }
+
+    return HAL_OK;
+}
+
+HAL_StatusTypeDef INA226_ReadCurrent(float *current)
+{
+    uint8_t raw_current[2]; // Temporary buffer for 2 bytes
+
+    HAL_StatusTypeDef status = HAL_I2C_Mem_Read(&hi2c1, INA226_I2C_ADDRESS << 1, 0x04, I2C_MEMADD_SIZE_8BIT, raw_current, 2, 10); 
+
+    if (status != HAL_OK)
+    {
+        return status; // Return the HAL status code if the read operation fails
+    }
+
+    // Current in mA
+    *current = ((int16_t)((raw_current[0] << 8) | raw_current[1])) * CURRENT_LSB * 1000; // Signed integer because the way current is flowing matters
+
+    return HAL_OK;
 }
