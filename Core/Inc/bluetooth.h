29463fbd71d97701a99c382d801e6f82c522769f
Sean Chiaramonte <seanchiaramonte@gmail.com>
2026-08-18 08:41:57 -0400

    feat(bluetooth): implement Bluetooth_Update() function
    - Check the status of commandReady; if false, the function returns; if true, the function continues
    
    - Copy commandBuffer into a local buffer to prevent read/write conflicts
    
    - Assign the RPM value to targetRPM using the mutex if the local buffer holds RPM and it's within the RPM range. If it holds START, set motorEN to 1 and systemFault to 0, and if it holds STOP, set motorEN to 0

--- Core/Inc/bluetooth.h
+++ Core/Inc/bluetooth.h
@@ -1,8 +1,11 @@
 #ifndef BLUETOOTH_H
 #define BLUETOOTH_H
 
-#include <stdint.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 #include "usart.h"
+#include "cmsis_os.h"
 
 /** 
  * @brief Arms the interrupt for one byte and returns immediately.
@@ -20,4 +23,16 @@ HAL_StatusTypeDef Bluetooth_Initialize(void);
 // No doxygen documentation needed for HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
 // It's already declared in stm32f4xx_hal_uart.h and it's only ever called by HAL
 
+/** 
+ * @brief Reads and handles an incoming bluetooth command.
+ * 
+ * @details 
+ * Checks if commandReady is true; if false, the function returns. If true, commandBuffer copies its information onto a new 
+ * local buffer. If that local buffer is holding an RPM value, then check if that RPM is within the correct range, if it isn't 
+ * the command is ignored; if it is, it's assigned to targetRPM with mutex protection. If the local buffer is holding "START" 
+ * then motorEN is set to 1 and systemFault is set to 0. If the local buffer is holding "STOP" then motorEN is set to 0.
+ * 
+ * @note Called every 20ms by the Bluetooth task. Bluetooth_Initialize() must be called first.
+*/
+void Bluetooth_Update(void);
 #endif /* BLUETOOTH_H */
--- Core/Src/bluetooth.c
+++ Core/Src/bluetooth.c
@@ -2,9 +2,14 @@
 
 static volatile char bluetoothBuffer[64]; // Buffer size is well above max amount of characters that will likely be sent (10-15)
 static volatile uint8_t bluetoothIndex = 0; 
-static volatile char commandBuffer[64]; // Completed line gets copied here once terminator arrives (ex. RPM: 500)
-static volatile uint8_t byteBuffer; // Buffer of one byte, single bytes are written to this buffer as they are received from the python app via bluetooth
+static volatile char commandBuffer[64]; // Completed line gets copied here once terminator arrives (ex. RPM:500)
+static uint8_t byteBuffer; // Buffer of one byte, single bytes are written to this buffer as they are received from the python app via bluetooth
 static volatile uint8_t commandReady = 0; 
+
+extern volatile float targetRPM;
+extern volatile uint16_t motorEN;
+extern volatile uint16_t systemFault;
+extern osMutexId_t motorStateMutexHandle;
 
 HAL_StatusTypeDef Bluetooth_Initialize(void)
 {
@@ -45,4 +50,38 @@ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
     }
 
     HAL_UART_Receive_IT(&huart3, &byteBuffer, 1); // Interrupt re-arming
+}
+
+void Bluetooth_Update(void)
+{
+    if (commandReady == 0) {
+        return;
+    }
+     
+    char localBuffer[64];
+    memcpy(localBuffer, (char*)commandBuffer, 64); // localBuffer created because commandBuffer can be written by HAL_UART_RxCpltCallback while its being read
+    commandReady = 0;
+
+    if (strncmp(localBuffer, "RPM:", 4) == 0) { // Compares the first 4 characters of localBuffer against RPM: which should match
+        
+        float localRPM = atof(localBuffer + 4); // Pointer to 4 characters into localBuffer where the number starts
+
+        if (localRPM >= 0.0f && localRPM <= 1000.0f) { // 1000 is a placeholder until actual RPM max is measured
+            osMutexAcquire(motorStateMutexHandle, osWaitForever);
+            targetRPM = localRPM; 
+            osMutexRelease(motorStateMutexHandle);
+        }
+
+    } else if (strcmp(localBuffer, "START") == 0) { // If Python app sends START
+        osMutexAcquire(motorStateMutexHandle, osWaitForever);
+        motorEN = 1;
+        systemFault = 0;
+        osMutexRelease(motorStateMutexHandle);
+
+    } else if (strcmp(localBuffer, "STOP") == 0) { // If Python app sends STOP
+        osMutexAcquire(motorStateMutexHandle, osWaitForever);
+        motorEN = 0;
+        osMutexRelease(motorStateMutexHandle);
+    }
+        
 }
